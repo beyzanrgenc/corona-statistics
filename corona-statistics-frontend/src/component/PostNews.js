b6b7b37fe47d917fd49a03e18bb84cf0c4e06e35
beyzanurgenc <beyzanurgenc@gmail.com>
2021-01-29 20:12:35 +0300

    post method for statistics implemented

--- corona-statistics-frontend/src/component/PostNews.js
+++ corona-statistics-frontend/src/component/PostNews.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../App.css';
 import PostNewsForm from './PostNewsForm';
 import City from '../data/city';
 import Keyword from '../data/keyword'
 
+const REST_API_URL = 'http://localhost:8081/saveStatistics';
+
 class PostNews extends Component {
 
+  async postNewsStatistics(value){
+    var postData = this.parseNews(value);
+    const response = await axios.post(
+      REST_API_URL, postData,
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+    console.log(response.data)
+  }
 
   parseNews(value) {
     var newsText = value;
@@ -17,33 +28,50 @@ class PostNews extends Component {
     while(sentenceArrayIter < sentenceArray.length){
       sentence = sentenceArray[sentenceArrayIter];
       if(date === null){
-        date = sentence.match(/\d{2}([.])\d{2}\1\d{4}/g);
+        date = sentence.match(/\d{2}([.])\d{2}\1\d{4}/g)[0];
+        sentence = sentence.replace(date,"");
       }
       if(covidCase === null){
-        if(sentence.indexOf(Keyword.Vaka.name) != -1){
-          covidCase = sentence.match(/\d+/g);
+        if(sentence.indexOf(Keyword.Vaka.name) !== -1){
+          covidCase = sentence.match(/\d+/g)[0];
         }
       }
       if(death === null){
-        if(sentence.indexOf(Keyword.Vefat.name) != -1){
-          death = sentence.match(/\d+/g);
+        if(sentence.indexOf(Keyword.Vefat.name) !== -1){
+          death = sentence.match(/\d+/g)[0];
         }
       }
       if(discharge === null){
-        if(sentence.indexOf(Keyword.Taburcu.name) != -1){
-          discharge = sentence.match(/\d+/g);
+        if(sentence.indexOf(Keyword.Taburcu.name) !== -1){
+          discharge = sentence.match(/\d+/g)[0];
         }
       }
       sentenceArrayIter++;
     }
-    return newTextLower;
+    date = date.substr(3, 2)+"."+date.substr(0, 2)+"."+date.substr(6, 4);
+    var currentDate = new Date();
+
+    var postStatistics = {
+      city: '', 
+      date: new Date(date),
+      covidCase: covidCase,
+      death: death,
+      discharge: discharge,
+      news: 
+      {
+        newsText: newsText,
+        newsDate: new Date(currentDate.getFullYear(),currentDate.getMonth(),currentDate.getDate(),currentDate.getHours(),currentDate.getMinutes(),currentDate.getSeconds())
+      }
+    }
+    console.log(postStatistics);
+    return postStatistics;
   }
 
 
   render() {
     return (
       <div className="PostNews">
-        <PostNewsForm onSubmitNews={this.parseNews.bind(this)} />
+        <PostNewsForm onSubmitNews={this.postNewsStatistics.bind(this)} />
       </div>
     )
   }
--- corona-statistics-server/src/main/java/com/project/corstat/controller/StatisticsController.java
+++ corona-statistics-server/src/main/java/com/project/corstat/controller/StatisticsController.java
@@ -3,10 +3,12 @@ package com.project.corstat.controller;
 import com.project.corstat.model.Statistics;
 import com.project.corstat.repository.StatisticsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class StatisticsController {
 
